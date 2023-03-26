094b65f8ce8c3e7411f6d0e6489a829822f72bbb
omer-tsr <127774093+omer-tsr@users.noreply.github.com>
2023-03-26 16:37:36 +0300

    Add files via upload
    7 10 ve 11. satırdakiler çalışmaz

---
+++ denetleyici.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int __xyz5;
+    int _xyz5;
+    short xyz = 34452;
+    int xyz ='*';
+    int xyz = 34452;
+    char xyz ='\192';
+    bool xyz = -1
+    int xyz=5.2;
+    float xyz = 12345.12345;
+
+
+    return 0;
+}
